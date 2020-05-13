61e4a4bab41a81c1a919b691638d990dcb61e068
ninjaaung <ninja.aung02@gmail.com>
2020-05-13 03:47:30 -0700

    Added pub_data and question text field form

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Question
 
-admin.site.register(Question)
-
 class QuestionAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question_text']
 
